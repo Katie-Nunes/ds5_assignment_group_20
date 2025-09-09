6eebb9215a829c7f3603a0378c7a5c0820af6b44
Katie <538658@student.fontys.nl>
2025-09-09 16:21:19 +0200

    Fix formatting slightly and task distribution

--- Week 2/main.py
+++ Week 2/main.py
@@ -1,17 +1,31 @@
+# TASKS:
+# Pick whichever task you want, then add the changes, then commit and push, and clarify which one you did
+# - Add functions
+# - Add Type hints
+# - Add docstrings
+
+import csv
+
 file_path = input("Enter the path to the CSV file: ")
 records = []
+
 with open(file_path, 'r') as file:
-csv_reader = csv.DictReader(file)
-for row in csv_reader:
-records.append(row)
+    csv_reader = csv.DictReader(file)
+    for row in csv_reader:
+        records.append(row)
+
 total = sum(float(record['Grade']) for record in records)
 average = total / len(records)
+
 print(f"Average Grade: {average}")
 print("--------------------")
+
 filtered_records = [record for record in records if float(record['Grade']) >= 80.0]
+
 print("Student Report")
 print("--------------")
+
 for record in filtered_records:
-print(f"Name: {record['Name']}")
-print(f"Grade: {record['Grade']}")
-print("--------------------")
+    print(f"Name: {record['Name']}")
+    print(f"Grade: {record['Grade']}")
+    print("--------------------")
