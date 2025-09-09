68dee62f15e23d63b56512359749b9c0239a61a0
Katie <538658@student.fontys.nl>
2025-09-09 22:45:20 +0200

    WIP outline for Exericise two

---
+++ Week 2/ExerciseTwo.py
@@ -0,0 +1,14 @@
+# This is some psuedocode I wrote for this exercise, it is very very rough,
+# I will make a clearer outline for this later
+
+# draw_mandel(width)
+
+	# Calculate_Mandel
+		# define_mandel
+			# x_range = [-1.5, 0.5]
+			# y_range =  [-1, 1]
+		# define_div_index
+		# calculate_div_index_per
+	# Visualize_Mandel
+		# Create Plot in right size
+		# Populate plot with pixels depending on div_index
