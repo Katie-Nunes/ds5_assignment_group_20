eecbc1cd065c99637b8752801fed533cb4c58d06
Aarrronnn <527594@student.fontys.nl>
2025-09-15 12:42:03 +0200

    Mapping pixel coordinates to complex plane points

--- Week 2/ExerciseTwo.py
+++ Week 2/ExerciseTwo.py
@@ -12,3 +12,13 @@
 	# Visualize_Mandel
 		# Create Plot in right size
 		# Populate plot with pixels depending on div_index
+
+
+def map_pixel_to_complex(i: int,j: int, width: int, x_range: tuple(float,float) = (-1.5,0.5),y_range: tuple(float,float) = (-1,1)) -> complex:
+    ## Maps a pixel coordinate (i,j) to a point in the complex plane
+    
+	x_min,x_max = x_range
+	y_min,y_max = y_range
+	x = x_min + (j / width) * (x_max - x_min)
+	y = y_min + (i / width) * (y_max - y_min)
+	return complex(x,y)
