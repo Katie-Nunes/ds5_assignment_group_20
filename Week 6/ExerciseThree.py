be412e6170e859d4fa30cbd95dfdbd743ccaceb0
halaelazabb-code <halaelazabb@gmail.com>
2025-10-22 14:52:15 +0200

    Create ExerciseThree.py

---
+++ Week 6/ExerciseThree.py
@@ -0,0 +1,6 @@
+#hala
+
+# First, the model might perform well on training data by memorizing patterns specific to that data,
+# but fail to generalize to new, unseen data. A test set provides an unbiased evaluation of the model's true performance. 
+# Also, the test set performance metrics (like R², accuracy, or mean IOU) give us a realistic
+# expectation of how the model will perform when deployed.
